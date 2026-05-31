2883f7a8d2660a2fa94ae5812a287ef593fe0fc9
Oliwier Szmyt <miss.lunar32@gmail.com>
2026-05-31 16:54:11 +0200

    works but undefined shows instead of name

--- tcgsite.php
+++ tcgsite.php
@@ -4,14 +4,12 @@ use GuzzleHttp\Client;
 
 $client = new Client();
 $query = $_GET['name'] ?? $_GET['id'] ?? 'celebi'; // celebi va jamais etre utilisé de base mais juste placeholder 
-$response = $client->request('GET',"https://pokeapi.co/api/v2/pokemon/$name"); // url api de pokemon de pokeapi 
+$response = $client->request('GET',"https://pokeapi.co/api/v2/pokemon/$query"); // url api de pokemon de pokeapi 
 $data = json_decode($response->getBody(), true); // utilisation du json 
 
-//$sprite = $data['sprites']["versions"]["generation-ii"]["crystal"]["front_default"] // les front-sprites de pokemon cristal
-//    ?? $data['sprites']['front_default']; // sinon les sprites par defaut si il n'y a pas de sprite gen2 (pokemon #252+)
 $sprite = $data['sprites']["front_default"]; // pas envie de me battre avec les images non-transparentes de la gen2
 echo json_encode([
-    'name' => $data['name'],
+    'query' => $data['name'],
     'id' => $data['id'],
     'sprite'=> $sprite
 ]);
